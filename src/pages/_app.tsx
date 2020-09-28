ff83ddaa82ffa59ad045d63fa5baadd771376096
Carlos Precioso <kalprestito@gmail.com>
2020-09-28 13:37:04 +0200

    Add WebMonetization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app"
+import Head from "next/head"
 import { FunctionComponent } from "react"
 import { GoogleAnalytics, IconTheming, SiteMetadata } from "../components/Seo"
 
@@ -7,6 +8,9 @@ const LastifyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
     <IconTheming />
     <SiteMetadata />
     <GoogleAnalytics />
+    <Head>
+      <meta name="monetization" content="$precioso.design" />
+    </Head>
     <Component {...pageProps} />
   </>
 )
