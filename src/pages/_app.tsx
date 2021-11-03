437ab4933c83eff398920e8857cc1455820278f0
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-11-03 14:26:29 +0100

    Remove WebMonetization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next"
 import type { AppProps } from "next/app"
-import Head from "next/head"
 import { GoogleAnalytics, IconTheming, SiteMetadata } from "../components/Seo"
 
 const LastifyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
@@ -8,9 +7,6 @@ const LastifyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
     <IconTheming />
     <SiteMetadata />
     <GoogleAnalytics />
-    <Head>
-      <meta name="monetization" content="$precioso.design" />
-    </Head>
     <Component {...pageProps} />
   </>
 )
